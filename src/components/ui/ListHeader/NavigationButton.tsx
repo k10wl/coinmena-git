95250f69bb96b47036a4caf5c7fb18314e58eae3
Yaroslav <yaroslav_kunytskyy@pecodesoftware.com>
2021-11-08 21:48:20 +0200

    Tweaked styling on navigation buttons

--- src/components/ui/ListHeader/NavigationButton.tsx
+++ src/components/ui/ListHeader/NavigationButton.tsx
@@ -8,6 +8,8 @@ const NavigationButtons = styled.button<Props>`
   background-color: ${(props) => (props.selected ? "#1f6feb" : "#161b22")};
   outline: none;
   border: none;
+  border: ${(props) =>
+    props.selected ? "1px solid #1f6feb" : "1px solid #30363d;"};
   &,
   & > * {
     color: ${(props) => (props.selected ? "#f0f6fc" : "#b4bcc3")};
@@ -16,9 +18,11 @@ const NavigationButtons = styled.button<Props>`
   }
   &:first-child {
     border-radius: 10px 0 0 10px;
+    border-right: none;
   }
   &:last-child {
     border-radius: 0 10px 10px 0;
+    border-left: none;
   }
   &:hover {
     cursor: pointer;
