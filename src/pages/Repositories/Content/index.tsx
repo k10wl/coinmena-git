cf95539086f8b929a688cd4b3a299e5dc0e44287
Yaroslav <yaroslav_kunytskyy@pecodesoftware.com>
2021-11-11 00:31:38 +0200

    Moved Button to UI to make it reusable

--- src/pages/Repositories/Content/index.tsx
+++ src/pages/Repositories/Content/index.tsx
@@ -7,7 +7,10 @@ import Fork from "@images/Fork.svg";
 import Star from "@images/Star.svg";
 import Repository from "@images/Repository.svg";
 
+import ListButton from "@ui/ListButton";
+
 import RepositoryInterface from "@/components/interface/repository";
+
 
 const Container = styled(Mui.Grid)`
   padding: 15px;
@@ -37,28 +40,6 @@ const RepoNameContainer = styled(Mui.Grid)`
     a {
       text-decoration: underline;
     }
-  }
-`;
-
-const Button = styled.button`
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  background-color: #21262d;
-  border: 1px solid #373b42;
-  color: #c9d1d9;
-  padding: 3px 12px;
-  border-radius: 5px;
-  img {
-    margin-right: 4px;
-  }
-  &:hover {
-    background-color: #30363d;
-    border-color: #8b949e;
-  }
-  &:active {
-    background-color: #282e33;
-    border-color: #6e7681;
   }
 `;
 
@@ -107,9 +88,9 @@ const Content = ({ repositoryInfo }: Props) => {
             </a>
           </RepoNameContainer>
         </Mui.Grid>
-        <Button>
+        <ListButton>
           <Icon spacing={2} src={Star} alt="star" /> Star
-        </Button>
+        </ListButton>
       </Mui.Grid>
       <RepoDetailsText marginY="4px" fontSize="12px" fontFamily="Segoe UI">
         {description}
