65e9326b30197acdbbccf5481918224ce1c9f252
Yaroslav <yaroslav_kunytskyy@pecodesoftware.com>
2021-11-11 11:10:38 +0200

    Switched to redux data in Developers and Repositories components

--- src/pages/Developers/index.tsx
+++ src/pages/Developers/index.tsx
@@ -1,43 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { useQuery } from "react-query";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 
 import BasePage from "@/components/ui/BasePage";
 import Content from "./Content";
 
-import DeveloperInterface from "@/components/interface/developer";
 import { RootState } from "@/store";
-import { storeDevData } from "@/store/developers";
 
 const Developers = () => {
   const { developers } = useSelector((state: RootState) => state);
-  const [developersApi, setDevelopersApi] = useState<DeveloperInterface[]>(developers);
-
-  const dispatch = useDispatch();
-
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  const targetUrl = "https://gh-trending-api.herokuapp.com/developers";
-
-  useEffect(() => {
-    setDevelopersApi(developers);
-  }, [developers]);
-
-  useQuery("devData", async () => {
-    if (developersApi.length === 0) {
-      try {
-        const data = await fetch(proxyUrl + targetUrl);
-        const response = await data.json();
-        dispatch(storeDevData(response));
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  });
 
   return (
     <BasePage>
       <>
-        {developersApi.map((developer) => (
+        {developers.map((developer) => (
           <Content key={developer.username} developer={developer} />
         ))}
       </>
