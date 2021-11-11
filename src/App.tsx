a2b6c479df270aeaf3448fdd61c3683523d9e689
Yaroslav <yaroslav_kunytskyy@pecodesoftware.com>
2021-11-11 11:11:04 +0200

    Added data fetching hook to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import useFetch from "@hooks/useFetch";
 
 import Developers from "./pages/Developers";
 import Repositories from "./pages/Repositories";
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<Repositories />} />
-    <Route path="/developers" element={<Developers />} />
-  </Routes>
-);
+const App = () => {
+  const { isLoading, error } = useFetch();
+
+  if(isLoading) {
+    return <h1>LOADING...</h1>
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<Repositories />} />
+      <Route path="/developers" element={<Developers />} />
+    </Routes>
+  );
+};
 
 export default App;
