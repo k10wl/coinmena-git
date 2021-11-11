5fabe3d9f40390e43444f7c4e869d315feff71a0
Yaroslav <yaroslav_kunytskyy@pecodesoftware.com>
2021-11-11 10:32:02 +0200

    Configured routing for Developers

--- src/pages/Developers/index.tsx
+++ src/pages/Developers/index.tsx
@@ -1,29 +1,43 @@
 import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { useSelector, useDispatch } from "react-redux";
 
 import BasePage from "@/components/ui/BasePage";
 import Content from "./Content";
 
 import DeveloperInterface from "@/components/interface/developer";
+import { RootState } from "@/store";
+import { storeDevData } from "@/store/developers";
 
 const Developers = () => {
-  const [developers, setDevelopers] = useState<DeveloperInterface[]>([]);
+  const { developers } = useSelector((state: RootState) => state);
+  const [developersApi, setDevelopersApi] = useState<DeveloperInterface[]>(developers);
+
+  const dispatch = useDispatch();
+
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const targetUrl = "https://gh-trending-api.herokuapp.com/developers";
+
   useEffect(() => {
-    var proxyUrl = "https://cors-anywhere.herokuapp.com/",
-      targetUrl = "https://gh-trending-api.herokuapp.com/developers";
-    fetch(proxyUrl + targetUrl)
-      .then((blob) => blob.json())
-      .then((data) => {
-        console.log('data:', data)
-        setDevelopers(data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }, []);
+    setDevelopersApi(developers);
+  }, [developers]);
+
+  useQuery("devData", async () => {
+    if (developersApi.length === 0) {
+      try {
+        const data = await fetch(proxyUrl + targetUrl);
+        const response = await data.json();
+        dispatch(storeDevData(response));
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  });
+
   return (
     <BasePage>
       <>
-        {developers.map((developer) => (
+        {developersApi.map((developer) => (
           <Content key={developer.username} developer={developer} />
         ))}
       </>
