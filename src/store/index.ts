0f43fb9f234105eea3e981d98a1ca3bcaf6b1ee5
Yaroslav <yaroslav_kunytskyy@pecodesoftware.com>
2021-11-11 10:31:46 +0200

    Created redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,15 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
 import repositories from "./repositories";
 import developers from "./developers";
+
+import RepositoryInterface from "@/components/interface/repository"
+import DevelopersInterface from "@/components/interface/developer"
+
+export interface RootState {
+  repositories: RepositoryInterface[];
+  developers: DevelopersInterface[];
+}
 
 const reducer = combineReducers({
   repositories: repositories.reducer,
